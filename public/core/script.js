bdeed5b11ae086cac02efeaeb7963265e2f1dccd
Samuel Hernandez <shamuel14g@gmail.com>
2026-08-23 15:50:05 -0500

    Acceso modulo accounts

--- public/core/script.js
+++ public/core/script.js
@@ -26,7 +26,7 @@ const ZYLOX_CONFIG = {
 
         // Fila 3
         { id: 'categories', name: 'Categorías', icon: 'tree', color: 'azure', file: 'categories.html' },
-        { id: 'locked', name: 'Bloqueado', icon: 'lock', color: 'locked', file: null }
+        { id: 'accounts', name: 'Cuentas', icon: 'wallet', color: 'holo', file: 'accounts.html' }
     ]
 };
 
@@ -39,7 +39,8 @@ const ICONS = {
     magnet: '<path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>',
     atom: '<path d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"/>',
     tree: '<path d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"/>',
-    lock: '<path d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>'
+    lock: '<path d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>',
+    wallet: '<path d="M3 7a2 2 0 012-2h14a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V7zm14 4a1 1 0 100 2 1 1 0 000-2z"/><path d="M3 7l9-4 9 4"/>'
 };
 
 // ... (Tailwind Config igual) ...
